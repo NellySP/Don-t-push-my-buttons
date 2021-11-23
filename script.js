1a867618658b084f5c48b4e47c53f9d8063c9c19
NellySP <nelly.petren@gmail.com>
2021-11-23 19:03:14 +0100

    added script to buttons

---
+++ script.js
@@ -0,0 +1,16 @@
+function moveElmRand(elm) {
+  elm.style.position = "absolute";
+  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+}
+
+//get the #btn_test
+const btn_push = document.querySelector("#bouncing");
+btn_push.addEventListener("click", function (e) {
+  moveElmRand(e.target);
+});
+
+const btn_touch = document.querySelector("#run");
+btn_touch.addEventListener("mouseover", function (e) {
+  moveElmRand(e.target);
+});
