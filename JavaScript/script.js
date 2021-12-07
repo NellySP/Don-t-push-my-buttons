78fd1fd04acbbdc740194d5dfb2813731bdb6293
NellySP <nelly.petren@gmail.com>
2021-12-07 12:07:45 +0100

    to double

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,34 +2,34 @@
 
 const objections = [
   "i said don't",
-  "cut it out ...",
-  "seriously??",
+  'cut it out ...',
+  'seriously??',
   "that's so immature",
-  "dONT!",
-  "sTOP IT 😡",
+  'dONT!',
+  'sTOP IT 😡',
   "your'e getting on my nerves",
   "i'm warning you!",
-  "get your own damn buttons!",
+  'get your own damn buttons!',
   "i mean it, don't do it!",
   "Don't force me to call the police",
   "this is a crime. I'll get you put in prison.",
-  "go push your own buttons",
-  "you can go push your mom or something...",
-  "would you please stop?!",
-  "this is ridiculus",
+  'go push your own buttons',
+  'you can go push your mom or something...',
+  'would you please stop?!',
+  'this is ridiculus',
   "don't even think about it",
-  "stop treating me like this",
+  'stop treating me like this',
   "some people just don't have any respect for others...",
   "why won't you stop? 😭",
-  "🤬",
-  "i am politely asking you to stop",
-  "Would you stop that?",
-  "stop being an asshat 🤠 ",
-  "Have you ever tried to stop? 🙃",
-  "you better stop",
-  "okay, you can keep pushing 😘",
+  '🤬',
+  'i am politely asking you to stop',
+  'Would you stop that?',
+  'stop being an asshat 🤠 ',
+  'Have you ever tried to stop? 🙃',
+  'you better stop',
+  'okay, you can keep pushing 😘',
   "this is honestly so rude, I can't understand why you would do this to me. Are you a bad person? You must be. At least you clearly have no concept of bounderies. The disrespect.",
-  "this is getting old",
+  'this is getting old',
 ];
 
 // Format the objections to make the first letter uppercase.
@@ -56,33 +56,33 @@ function protest() {
 // Function to move the buttons around
 
 function moveElmRand(elm) {
-  elm.style.position = "absolute";
-  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.position = 'absolute';
+  elm.style.top = Math.floor(Math.random() * 90 + 5) + '%';
+  elm.style.left = Math.floor(Math.random() * 90 + 5) + '%';
   count++;
 }
 
 // Event listener scroll, trigger at window alert.
 
-window.addEventListener("scroll", (event) => {
-  alert("Yeah, you better scroll away...");
+window.addEventListener('scroll', (event) => {
+  alert('Yeah, you better scroll away...');
 });
 
 // Event listeners for the buttons
-const btn_push = document.querySelector("#bouncing");
-btn_push.addEventListener("click", function (e) {
+const btn_push = document.querySelector('#bouncing');
+btn_push.addEventListener('click', function (e) {
   moveElmRand(e.target);
   protest();
 });
 
-const btn_touch = document.querySelector("#run");
-btn_touch.addEventListener("mouseover", function (e) {
+const btn_touch = document.querySelector('#run');
+btn_touch.addEventListener('mouseover', function (e) {
   moveElmRand(e.target);
   protest();
 });
 
-const warning = document.getElementById("dont");
-warning.addEventListener("click", (e) => {
+const warning = document.getElementById('dont');
+warning.addEventListener('click', (e) => {
   moveElmRand(e.target);
   protest();
 });
