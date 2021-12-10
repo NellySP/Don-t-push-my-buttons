8a876b2f33b080d7689ba2f5dad811366daa61d0
NellySP <nelly.petren@gmail.com>
2021-12-10 10:23:14 +0100

    add splash screen

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,13 @@
+// Splash screen
+
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  setTimeout(() => {
+    splash.classList.add('display-none');
+  }, 2000);
+});
+
 // Array of objections
 
 const objections = [
